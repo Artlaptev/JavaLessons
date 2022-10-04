328fb068b03d8954bf2954d3b71e5dbc67f5e92a
temkalap <89065927+Artlaptev@users.noreply.github.com>
2022-10-04 09:14:44 +0300

    решена задача 1 без логирования

--- JavaLessons/src/Task1.java
+++ JavaLessons/src/Task1.java
@@ -1,2 +1,50 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Task1 {
+    public static void main(String[] args) {
+        File[] f=getContentByDirectoryPath("C:\\");
+        System.out.println(writeArrayInFile(f));
+    }
+
+    public static File[] getContentByDirectoryPath(String path) {
+        try {
+            File file = new File(path);
+            return file.listFiles();
+        } catch (NullPointerException e) {
+            return new File[0];
+        }
+    }
+
+    public static boolean writeArrayInFile(File[] array) {
+        StringBuilder sb=new StringBuilder();
+        for (File item:array) {
+            sb.append(item.toString()).append("\n");
+        }
+        File file = new File("out.txt");
+        FileWriter fileWriter=null;
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+                fileWriter=new FileWriter(file);
+                fileWriter.write(sb.toString());
+                fileWriter.flush();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return  false;
+            }finally {
+                try {
+                    fileWriter.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return  false;
+                }
+            }
+        }
+        return true;
+    }
+
 }
